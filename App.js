e22486821f937e6b1fe9687c71c005f19c87e156
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-10-21 01:17:14 +0900

    import Loading and Login Pages

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Loading from './pages/Loading';
+import Login from './pages/Login';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const  App = () =>{
+  const [isLoading,setIsLoading]=useState(false);
+  return isLoading?  <Loading></Loading> : <Login></Login>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
