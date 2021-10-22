89024709163e50b9c63e02767baac053c4b3b026
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-10-23 02:07:18 +0900

    set configuration and environ

--- api/app.js
+++ api/app.js
@@ -1,6 +1,20 @@
 const express=require('express');
 const app=express();
+const dotenv=require('dotenv');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const authRouter=require('./routes/auth');
+const {sequelize} = require('./models/index');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+sequelize.sync();
+dotenv.config();
+
+
 
 app.listen(8000,()=>{
     console.log("Api server open");
 });
+app.use('/auth',authRouter);
