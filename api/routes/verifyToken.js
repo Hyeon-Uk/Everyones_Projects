e26da4b1f1b17f2ea52773e3461bb4af6564b569
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-10-26 23:16:04 +0900

    add verifyTokenAndAuthorization

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const verifyUser = (req, res, next) => {
+const verifyToken = (req, res, next) => {
     const token = req
         .headers
         .token.split(" ")[1];
@@ -9,7 +9,7 @@ const verifyUser = (req, res, next) => {
             error && res
                 .status(403)
                 .json("Token is not valid");
-            req.user = user;
+            req.user=user;
             next();
         })
     } else {
@@ -19,6 +19,18 @@ const verifyUser = (req, res, next) => {
     }
 }
 
+const verifyTokenAndAuthorization=(req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if(req.user.u_id==req.params.u_id){
+            next();
+        }
+        else{
+            res.status(403).json("You are not allowed");
+        }
+    })
+}
+
+
 module.exports = {
-    verifyUser
+    verifyToken,verifyTokenAndAuthorization
 };
