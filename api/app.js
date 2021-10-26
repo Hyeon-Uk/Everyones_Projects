390cd5a335d41194ed5c0dd464f327a78d58af7b
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-10-26 23:13:45 +0900

    add cookieParser and userRouter

--- api/app.js
+++ api/app.js
@@ -3,13 +3,16 @@ const app=express();
 const dotenv=require('dotenv');
 const cors=require('cors');
 const bodyParser=require('body-parser');
+const cookieParser=require('cookie-parser');
 const authRouter=require('./routes/auth');
 const postRouter=require('./routes/post');
+const userRouter=require('./routes/user');
+
 const {sequelize} = require('./models/index');
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 sequelize.sync();
 dotenv.config();
 
@@ -20,3 +23,4 @@ app.listen(8000,()=>{
 });
 app.use('/auth',authRouter);
 app.use('/post',postRouter);
+app.use('/user',userRouter);
